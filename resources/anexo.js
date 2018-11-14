805aafb11bae7ef71f65af798a3ca01d51bf35ae
Nallely Rodriguez <nallely.rf89@gmail.com>
2018-11-14 16:22:41 -0600

    Dibujando en canvas con las flechas

--- resources/anexo.js
+++ resources/anexo.js
@@ -1,13 +1,28 @@
+/* Enlazar elementos generales de html */
 var numLineas=document.getElementById("numIngresado");
 var btnCalcular=document.getElementById("btnCalcular");
+
+
+/* LIENZO 1 */
 var	d=document.getElementById('areaDibujo');
-var lienzo = d.getContext('2d'); /* Indicar tipo de lienzo 2d o 3d*/
+var lienzo = d.getContext('2d'); 
+
+/* LIENZO 2*/
+var	f=document.getElementById('areaDibujo2');
+var lienzo2 = f.getContext('2d');
 
 
+
+
+/*       ESTABLECIENDO EVENTOS.       */
+document.addEventListener('keydown',dibujarTeclado);
 
 btnCalcular.addEventListener('click', dibujoPorClick);
 
 
+
+
+/*       FUNCIONES.       */
 
 function dibujoPorClick(){
 	var lineas=numLineas.value;
@@ -19,21 +34,71 @@ function dibujoPorClick(){
 	for (l = 0; l < lineas; l++) {
 		var yi	= espacio * l;
 		var xf = espacio * (l + 1);
-		dibujarLinea(color,0, yi,xf ,anchoLienzo);	
+		dibujarLinea(color,0, yi,xf ,anchoLienzo,lienzo);	
 	}
-	dibujarLinea(color,1,1,1,299);
-	dibujarLinea(color, 1,299,299,299);
+	dibujarLinea(color,1,1,1,299,lienzo);
+	dibujarLinea(color, 1,299,299,299,lienzo);
 }
 
 
 
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
-	lienzo.beginPath(); /* abrir el trazo a realizar */
-	lienzo.strokeStyle = color; /* Color de linea */
-	lienzo.moveTo( xinicial, yinicial); /* Posicion inicial del trazo */
-	lienzo.lineTo(xfinal, yfinal); /* Posicion final del trazo*/
-	lienzo.stroke(); /* Realiza trazo */
-	lienzo.closePath(); /* Cerrar trazo */
+
+var x=149;
+var y=149;
+movimiento=10;
+
+function dibujarTeclado(evento){
+	var tipoLienzo=lienzo2;
+	var teclas={
+		IZQUIERDA : 37,
+		ARRIBA : 38,
+		DERECHA : 39,
+		ABAJO : 40
+	};
+
+
+
+	switch (evento.keyCode) {
+		case teclas.IZQUIERDA:
+		dibujarLinea("red", x,y,x-movimiento,y,tipoLienzo);
+		x=x-movimiento
+		break;
+		case teclas.ARRIBA:
+		dibujarLinea("pink", x,y,x,y-movimiento,tipoLienzo);
+		y=y-movimiento;
+		break;
+		case teclas.DERECHA:
+		dibujarLinea("green", x,y,x+movimiento,y,tipoLienzo);
+		x=x+movimiento
+		break;
+		case teclas.ABAJO:
+		dibujarLinea("blue", x,y,x,y+movimiento,tipoLienzo);
+		y=y+movimiento;
+		break;
+		default:
+		
+		break;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal,areaDibujante){
+	areaDibujante.beginPath(); /* abrir el trazo a realizar */
+	areaDibujante.strokeStyle = color; /* Color de linea */
+	areaDibujante.lineWidth = 5;
+	areaDibujante.moveTo( xinicial, yinicial); /* Posicion inicial del trazo */
+	areaDibujante.lineTo(xfinal, yfinal); /* Posicion final del trazo*/
+	areaDibujante.stroke(); /* Realiza trazo */
+	areaDibujante.closePath(); /* Cerrar trazo */
 }
 
 
